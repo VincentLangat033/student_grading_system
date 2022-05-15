16a38958f3d4fb953fda17949e74ea82e57bbe4c
Vincent Langat <kimutailangat033@gmail.com>
2022-05-15 08:52:23 +0300

    commit will add responsivenes to the submit button

--- index.js
+++ index.js
@@ -14,29 +14,38 @@ function yourScore(){
   display.style.display="visible"
 
 
-    if(quiz1=="B"){ result++ }
+    if(quiz1=="D"){ result++ }
     if(quiz2=="A"){ result++ }
     if(quiz3=="A"){ result++ }
     if(quiz4=="A"){ result++ }
-    if(quiz5=="A"){ result++ }
+    if(quiz5=="B"){ result++ }
 
 
     if(quiz1=="" || quiz2=="" || quiz3=="" || quiz4=="" || quiz5=="" ){
         alert("Please Answer All the questions")
     }
-    var finalScore= result*33.3;
-    if( finalScore>=70){
+    var finalScore= result*20;
+    if( finalScore>=80){
         displayForm.innerHTML= "Your Score is " + finalScore + "%" + "Excellent"
 
     }
-    else if( finalScore>=33 && finalScore<70){
+    else if( finalScore>=60 && finalScore<80){
         displayForm.innerHTML= "Your Score is " + finalScore + "%" + "Fairly Done"
 
     }
-    else if ( finalScore>=0 && finalScore<33){
+    else if ( finalScore>=40 && finalScore<60){
+        displayForm.innerHTML="Your Score is " + finalScore + "%" + "Not Bad"
+
+    }
+    else if ( finalScore>=0 && finalScore<40){
         displayForm.innerHTML="Your Score is " + finalScore + "%" + "Try Again!"
 
     }
     
 
+}
+function newComments(){
+   var viewComments=document.getElementById("viewComments")
+   viewComments.innerHTML="It is here"
+   
 }
